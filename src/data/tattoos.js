a3a3eb05a9561645fa506bb9e0874273017a12ad
Minst199x <Minst199x@users.noreply.github.com>
2021-02-17 12:00:35 +0700

    success

---
+++ src/data/tattoos.js
@@ -0,0 +1,14 @@
+const tattoos = [
+  { title: "แสนสวย 01", thumbnailUrl: "/images/1.jpg" },
+  { title: "แสนสวย 02", thumbnailUrl: "/images/2.jpg" },
+  { title: "แสนสวย 03", thumbnailUrl: "/images/3.jpg" },
+  { title: "แสนสวย 04", thumbnailUrl: "/images/4.jpg" },
+  { title: "แสนสวย 05", thumbnailUrl: "/images/5.jpg" },
+  { title: "แสนสวย 06", thumbnailUrl: "/images/6.jpg" },
+  { title: "แสนสวย 07", thumbnailUrl: "/images/7.jpg" },
+  { title: "แสนสวย 08", thumbnailUrl: "/images/8.jpg" },
+  { title: "แสนสวย 09", thumbnailUrl: "/images/9.jpg" },
+  { title: "แสนสวย 10", thumbnailUrl: "/images/10.jpg" },
+];
+
+export default tattoos;
