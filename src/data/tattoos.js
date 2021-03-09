820ea3a1f8f049042ed5fae88ee4d1820d97d953
supermin199x <s.statiion@icloud.com>
2021-03-09 14:24:17 +0700

    fix img

--- src/data/tattoos.js
+++ src/data/tattoos.js
@@ -1,14 +1,14 @@
 const tattoos = [
-  { title: "แสนสวย 01", thumbnailUrl: "/images/1.jpg" },
-  { title: "แสนสวย 02", thumbnailUrl: "/images/2.jpg" },
-  { title: "แสนสวย 03", thumbnailUrl: "/images/3.jpg" },
-  { title: "แสนสวย 04", thumbnailUrl: "/images/4.jpg" },
-  { title: "แสนสวย 05", thumbnailUrl: "/images/5.jpg" },
-  { title: "แสนสวย 06", thumbnailUrl: "/images/6.jpg" },
-  { title: "แสนสวย 07", thumbnailUrl: "/images/7.jpg" },
-  { title: "แสนสวย 08", thumbnailUrl: "/images/8.jpg" },
-  { title: "แสนสวย 09", thumbnailUrl: "/images/9.jpg" },
-  { title: "แสนสวย 10", thumbnailUrl: "/images/10.jpg" },
+  { title: "แสนสวย 01", thumbnailUrl: "https://i.ibb.co/By6JfTw/1.jpg" },
+  { title: "แสนสวย 02", thumbnailUrl: "https://i.ibb.co/vxRVqG1/2.jpg" },
+  { title: "แสนสวย 03", thumbnailUrl: "https://i.ibb.co/5M1KtWZ/3.jpg" },
+  { title: "แสนสวย 04", thumbnailUrl: "https://i.ibb.co/ynjsDd1/4.jpg" },
+  { title: "แสนสวย 05", thumbnailUrl: "https://i.ibb.co/fCTQ2VT/5.jpg" },
+  { title: "แสนสวย 06", thumbnailUrl: "https://i.ibb.co/qM2yDJG/6.jpg" },
+  { title: "แสนสวย 07", thumbnailUrl: "https://i.ibb.co/PzGw0k4/7.jpg" },
+  { title: "แสนสวย 08", thumbnailUrl: "https://i.ibb.co/6WQv521/8.jpg" },
+  { title: "แสนสวย 09", thumbnailUrl: "https://i.ibb.co/LSrftbk/9.jpg" },
+  { title: "แสนสวย 10", thumbnailUrl: "https://i.ibb.co/BghrXTP/10.jpg" },
 ];
 
 export default tattoos;
